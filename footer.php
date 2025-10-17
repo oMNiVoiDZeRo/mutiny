bd8e06f7c16da2c5cddc9381625b31bb139ee2f4
omnivoidzero <dan@reflectecho.com>
2025-10-17 12:04:00 -0700

    Converted database connection messages into html comments.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <?php
 echo '<br/>';
 mysqli_close($conn);
-if($conn !== true){echo '<center><em>Database Successfully Disconnected.</em></center>';}
+if($conn !== true){echo '<!-- Database Successfully Disconnected. -->';}
 ?>
--- header.php
+++ header.php
@@ -4,6 +4,6 @@ $user = 'root';
 $pass = '';
 $database = 'mutiny';
 $conn = mysqli_connect($server, $user, $pass, $database);
-if($conn == true){echo '<center><em>Database Successfully Connected.</em></center>';}
+if($conn == true){echo '<!-- Database Successfully Connected. -->';}
 echo '<br/>';
 ?>
